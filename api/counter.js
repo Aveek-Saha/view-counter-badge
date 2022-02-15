54463227b4d5a88fa1f5635dd72562f1e153910b
Aveek Saha <aveek.s98@gmail.com>
2022-02-15 19:46:54 +0530

    Create badge

--- api/counter.js
+++ api/counter.js
@@ -1,6 +1,7 @@
 module.exports = async (req, res) => {
     const { initializeApp, cert } = require("firebase-admin/app");
     const { getFirestore } = require("firebase-admin/firestore");
+    const { makeBadge, ValidationError } = require("badge-maker");
 
     // Fetch the service account key JSON file contents
     var serviceAccount = require("../serviceAccountKey.json");
@@ -14,14 +15,24 @@ module.exports = async (req, res) => {
     const counterRef = db.collection("counter").doc("views");
     const doc = await counterRef.get();
     if (!doc.exists) {
-        var views = { count: 1 };
+        var new_count = 1
+        var views = { count: new_count };
         await counterRef.set(views);
     } else {
         var views = doc.data();
-        var update_view = { count: views.count + 1 }
+        var new_count = views.count + 1
+        var update_view = { count: new_count };
         await counterRef.set(update_view);
     }
 
+    const format = {
+        label: "Views",
+        message: new_count.toString(),
+        color: "green",
+    };
+
+    const svg = makeBadge(format);
+
     const { name } = req.query;
-    res.status(200).send(`Views ${views.count}!`);
+    res.status(200).send(svg);
 };
