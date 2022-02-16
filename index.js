5c3416df5116176604cea7d5a311be761befdc90
Aveek Saha <aveek.s98@gmail.com>
2022-02-16 20:43:11 +0530

    Restore cloudflare

---
+++ index.js
@@ -0,0 +1,39 @@
+const { badgen } = require('badgen')
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+
+  var count = await ViewCounter.get('count')
+
+  if (count){
+    var views = parseInt(JSON.parse(count)) + 1
+    await ViewCounter.put('count', JSON.stringify(views));
+  }
+  else{
+    var views = 0
+    await ViewCounter.put('count', JSON.stringify(views));
+  }
+  var res = {
+      "schemaVersion": 1,
+      "label": "Views",
+      "message": views.toString(),
+      "color": "orange"
+    }
+
+    const svgString = badgen({
+      label: 'Views',      // <Text>
+      status: views.toString(),  // <Text>, required
+    })
+  // return new Response(JSON.stringify(res), {
+  //   headers: { 'content-type': 'application/json' },
+  // })
+  return new Response(svgString, {
+    headers: { 'content-type': 'image/svg+xml' },
+  })
+}
