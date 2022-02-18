95a0fd84b14ca1951b5466d58076d48c90df7058
Aveek Saha <aveek.s98@gmail.com>
2022-02-18 22:09:42 +0530

    Format code

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const { badgen } = require('badgen')
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 /**
  * Respond with svg badge
  * @param {Request} request
  */
 async function handleRequest(request) {
+    var count = await ViewCounter.get('count')
 
-  var count = await ViewCounter.get('count')
-
-  if (count){
-    var views = parseInt(JSON.parse(count)) + 1
-    await ViewCounter.put('count', JSON.stringify(views));
-  }
-  else{
-    var views = 0
-    await ViewCounter.put('count', JSON.stringify(views));
-  }
+    if (count) {
+        var views = parseInt(JSON.parse(count)) + 1
+        await ViewCounter.put('count', JSON.stringify(views))
+    } else {
+        var views = 0
+        await ViewCounter.put('count', JSON.stringify(views))
+    }
 
     const { searchParams } = new URL(request.url)
     let label = searchParams.get('label') || 'Views'
@@ -28,14 +26,14 @@ async function handleRequest(request) {
     let scale = searchParams.get('scale') || 1
 
     const svgString = badgen({
-      label: label,
-      labelColor: labelColor,
-      color: color,
-      style: style,
-      scale: scale,
-      status: views.toString(),
+        label: label,
+        labelColor: labelColor,
+        color: color,
+        style: style,
+        scale: scale,
+        status: views.toString(),
     })
-  return new Response(svgString, {
-    headers: { 'content-type': 'image/svg+xml' },
-  })
+    return new Response(svgString, {
+        headers: { 'content-type': 'image/svg+xml' },
+    })
 }
